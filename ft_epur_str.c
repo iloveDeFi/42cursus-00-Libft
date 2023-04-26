c60d4104856337bdefd8f5442771c4bde15dc8ff
Baptiste Bessard <bessardbusiness@gmail.com>
2023-04-26 10:50:37 +0200

    ft_epur_str.c

---
+++ ft_epur_str.c
@@ -0,0 +1,36 @@
+/*
+ * Fonction pour supprimer les espaces en trop
+ */
+
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+	int i;
+	int flag;
+
+	if (ac == 2)
+	{
+		while (av[1][i] == ' ' || av[1][i] == '\t')
+			i++;
+		int i = 0;
+		while (av[1][i])
+		{
+			if (av[1][i] == ' ' || av[1][i] == '\t')
+				flag = 1;
+			if (!(av[1][i] == ' ' || av[1][i] == '\t'))
+			{
+				if (flag == 1)
+				{
+					write (1, " ", 1);
+					flag = 0;
+				}
+				write (1, &av[1][i], 1);
+			}
+			i++;
+		}
+		return (0);
+	}
+	write (1, "\n", 1);
+}
+
