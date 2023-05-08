3dda4b261f3126313d11ad2bab7a346c5cb19077
Baptiste Bessard <bessardbusiness@gmail.com>
2023-05-08 11:11:18 +0200

    norminette

--- ft_fprime.c
+++ ft_fprime.c
@@ -13,28 +13,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    int nbr = atoi(av[1]);
+	int	nbr;
+	int	i;
 
-    if (ac == 2)
-    {
-        int i = 1;
-        if (nbr == 1)
-            printf("1");
-        while (++i <= nbr)
-        {
-            if (nbr % i == 0)
-            {
-                printf("%d", i);
-                if (nbr == i)
-                    break;
-                printf("*");
-                nbr /= i;
-                i = 1;
-        }
-        }
-    }
-    printf("\n");
-    return(0);
+	nbr = atoi(av[1]);
+	if (ac == 2)
+	{
+		i = 1;
+		if (nbr == 1)
+			printf("1");
+		while (++i <= nbr)
+		{
+			if (nbr % i == 0)
+			{
+				printf ("%d", i);
+				if (nbr == i)
+					break ;
+				printf("*");
+				nbr /= i;
+				i = 1;
+			}
+		}
+	}
+	printf ("\n");
+	return (0);
 }
